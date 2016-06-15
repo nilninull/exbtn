664c3de39fb276e26cd7b3234191f9a220832124
nilninull <nilninull@gmail.com>
2016-06-15 21:09:49 +0900

    wrap output codes by DEBUG

--- exbtn.c
+++ exbtn.c
@@ -124,12 +124,16 @@ void button_check(int fd, unsigned char* buf, const unsigned char btn_bit, const
       uinput_button_press(fd, key_code);
       btn_state |= btn_bit;
 
-      printf("btn 0x%02X pressed\n", btn_bit);
+#ifdef DEBUG
+      fprintf(stderr, "btn 0x%02X pressed\n", btn_bit);
+#endif /* Def: DEBUG */
    } else if (btn_state & btn_bit){
       uinput_button_release(fd, key_code);
       btn_state &= ~btn_bit;
 
-      printf("btn 0x%02X released\n", btn_bit);
+#ifdef DEBUG
+      fprintf(stderr, "btn 0x%02X released\n", btn_bit);
+#endif /* Def: DEBUG */
    }
 }
 
@@ -153,14 +157,15 @@ int main(int argc, char* argv[])
    devs = hid_enumerate(VendorID, ProductID);
    cur_dev = devs;	
    while (cur_dev) {
-      printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls", cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
-      printf("\n");
-      printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
-      printf("  Product:      %ls\n", cur_dev->product_string);
-      printf("  Release:      %hx\n", cur_dev->release_number);
-      printf("  Interface:    %d\n",  cur_dev->interface_number);
-      printf("\n");
-      
+#ifdef DEBUG
+      fprintf(stderr, "Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls", cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
+      fprintf(stderr, "\n");
+      fprintf(stderr, "  Manufacturer: %ls\n", cur_dev->manufacturer_string);
+      fprintf(stderr, "  Product:      %ls\n", cur_dev->product_string);
+      fprintf(stderr, "  Release:      %hx\n", cur_dev->release_number);
+      fprintf(stderr, "  Interface:    %d\n",  cur_dev->interface_number);
+      fprintf(stderr, "\n");
+#endif /* Def: DEBUG */      
       if (cur_dev->interface_number == 2) {
          strncpy(path, cur_dev->path, sizeof(path));
          break;
@@ -177,16 +182,17 @@ int main(int argc, char* argv[])
 
    handle = hid_open_path(path);
    if (!handle) {
-      printf("unable to open device\n");
+      fprintf(stderr, "unable to open device\n");
       return 1;
    }
 
    // Set the hid_read() function to be non-blocking.
-   hid_set_nonblocking(handle, 1);
+   /* hid_set_nonblocking(handle, 1); */
 
    do {
-      res = hid_read(handle, buf, sizeof(buf));
-      printf("flush: hid_read res is %d\n", res);
+      /* res = hid_read(handle, buf, sizeof(buf)); */
+      res = hid_read_timeout(handle, buf, sizeof(buf), 0);
+      fprintf(stderr, "flush: hid_read res is %d\n", res);
    } while (res != 0) ;
    
    
@@ -203,7 +209,7 @@ int main(int argc, char* argv[])
    while (1) {
       res = hid_read(handle, buf, sizeof(buf));
 
-      printf("hid_read res is %d\n", res);
+      fprintf(stderr, "hid_read res is %d\n", res);
       
       if (res != 16) continue;
 
