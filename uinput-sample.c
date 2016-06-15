f13c9ee331ffb403c207730fdee8c4d8aa0e09e5
nilninull <nilninull@gmail.com>
2016-06-15 12:58:22 +0900

    import sample program for uinput

--- uinput-sample.c
+++ uinput-sample.c
@@ -12,6 +12,8 @@
       exit(EXIT_FAILURE);                       \
    } while(0)
 
+#define UINPUT_PATH "/dev/uinput"
+
 int
 main(void)
 {
@@ -21,21 +23,29 @@ main(void)
    int                    dx, dy;
    int                    i;
 
-   fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+   fd = open(UINPUT_PATH, O_WRONLY | O_NONBLOCK);
    if(fd < 0)
       die("error: open");
 
    if(ioctl(fd, UI_SET_EVBIT, EV_KEY) < 0)
       die("error: ioctl");
-   if(ioctl(fd, UI_SET_KEYBIT, BTN_LEFT) < 0)
-      die("error: ioctl");
 
-   if(ioctl(fd, UI_SET_EVBIT, EV_REL) < 0)
-      die("error: ioctl");
-   if(ioctl(fd, UI_SET_RELBIT, REL_X) < 0)
-      die("error: ioctl");
-   if(ioctl(fd, UI_SET_RELBIT, REL_Y) < 0)
-      die("error: ioctl");
+   for(i = BTN_MISC; i <=  BTN_9; ++i) {
+      if(ioctl(fd, UI_SET_KEYBIT, i) < 0)
+         die("error: ioctl");
+   }
+
+   for(i = BTN_MOUSE; i <= BTN_TASK; ++i) {
+      if(ioctl(fd, UI_SET_KEYBIT, i) < 0)
+         die("error: ioctl");
+   }
+
+    if(ioctl(fd, UI_SET_EVBIT, EV_REL) < 0)
+        die("error: ioctl");
+    if(ioctl(fd, UI_SET_RELBIT, REL_X) < 0)
+        die("error: ioctl");
+    if(ioctl(fd, UI_SET_RELBIT, REL_Y) < 0)
+        die("error: ioctl");
 
    memset(&uidev, 0, sizeof(uidev));
    snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "uinput-sample");
@@ -49,68 +59,52 @@ main(void)
 
    if(ioctl(fd, UI_DEV_CREATE) < 0)
       die("error: ioctl");
-
+   
    sleep(2);
+    
+   for(i = BTN_MISC; i <= BTN_TASK; ++i) {
 
-   srand(time(NULL));
+      memset(&ev, 0, sizeof(struct input_event));
+      ev.type = EV_KEY;
+      ev.code = i;
+      ev.value = 1;
+      if(write(fd, &ev, sizeof(struct input_event)) < 0)
+         die("error: write");
 
-   while(1) {
-      switch(rand() % 4) {
-      case 0:
-         dx = -10;
-         dy = -1;
-         break;
-      case 1:
-         dx = 10;
-         dy = 1;
-         break;
-      case 2:
-         dx = -1;
-         dy = 10;
-         break;
-      case 3:
-         dx = 1;
-         dy = -10;
-         break;
-      }
+      /* memset(&ev, 0, sizeof(struct input_event)); */
+      /* ev.type = EV_KEY; */
+      /* ev.code = i; */
+      /* ev.value = 0; */
+      /* if(write(fd, &ev, sizeof(struct input_event)) < 0) */
+      /*    die("error: write"); */
 
-      for(i = 0; i < 20; i++) {
-         memset(&ev, 0, sizeof(struct input_event));
-         ev.type = EV_REL;
-         ev.code = REL_X;
-         ev.value = dx;
-         if(write(fd, &ev, sizeof(struct input_event)) < 0)
-            die("error: write");
+      memset(&ev, 0, sizeof(struct input_event));
+      ev.type = EV_SYN;
+      ev.code = 0;
+      ev.value = 0;
+      if(write(fd, &ev, sizeof(struct input_event)) < 0)
+         die("error: write");
 
-         memset(&ev, 0, sizeof(struct input_event));
-         ev.type = EV_REL;
-         ev.code = REL_Y;
-         ev.value = dy;
-         if(write(fd, &ev, sizeof(struct input_event)) < 0)
-            die("error: write");
+      sleep(2);
 
-         memset(&ev, 0, sizeof(struct input_event));
-         ev.type = EV_KEY;
-         ev.code = BTN_MISC;
-         ev.value = dy;
-         if(write(fd, &ev, sizeof(struct input_event)) < 0)
-            die("error: write");
+      memset(&ev, 0, sizeof(struct input_event));
+      ev.type = EV_KEY;
+      ev.code = i;
+      ev.value = 0;
+      if(write(fd, &ev, sizeof(struct input_event)) < 0)
+         die("error: write");
 
+      memset(&ev, 0, sizeof(struct input_event));
+      ev.type = EV_SYN;
+      ev.code = 0;
+      ev.value = 0;
+      if(write(fd, &ev, sizeof(struct input_event)) < 0)
+         die("error: write");
 
-         memset(&ev, 0, sizeof(struct input_event));
-         ev.type = EV_SYN;
-         ev.code = 0;
-         ev.value = 0;
-         if(write(fd, &ev, sizeof(struct input_event)) < 0)
-            die("error: write");
-
-         usleep(15000);
-      }
-
-      sleep(5);
+      sleep(2);
    }
-
-   sleep(2);
+    
+   /* usleep(15000); */
 
    if(ioctl(fd, UI_DEV_DESTROY) < 0)
       die("error: ioctl");
